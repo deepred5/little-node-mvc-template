b0993bece033448be46e951278904b74bc2f2535
deepred5 <948931744@qq.com>
2020-07-17 18:19:39 +0800

    update: port
    update: config

--- config/base.js
+++ config/base.js
@@ -1,13 +1,10 @@
 const env = process.env
 const httpPort = env.PORT || 8080
 const NODE_ENV = env.NODE_ENV || 'prod'
-const pkg = require('../package.json')
+
 module.exports = {
-	port: httpPort,
-	hostAddress: env.HOST_ADDRESS || '0.0.0.0',
-	nodeEnv: NODE_ENV,
-	log: {
-		projectName: pkg.name,
-		fileName: `/home/logs/${pkg.name}/logstash/`
-	}
+  port: httpPort,
+  hostAddress: env.HOST_ADDRESS || '0.0.0.0',
+  nodeEnv: NODE_ENV,
+  host: env.HOST,
 }
--- config/dev.js
+++ config/dev.js
@@ -1,14 +1,14 @@
 module.exports = {
-	proxy: [
-		{
-			match: '/x/web-interface/*',
-			config: {
-				target: 'https://api.bilibili.com',
-				changeOrigin: true,
-				onProxyReq(proxyReq, req, res) {
-					proxyReq.setHeader('referer', 'https://www.bilibili.com/'); // 防爬虫
-				}
-			}
-		}
-	]
-}
+  proxy: [
+    {
+      match: '/x/web-interface/*',
+      config: {
+        target: 'https://api.bilibili.com',
+        changeOrigin: true,
+        onProxyReq(proxyReq, req, res) {
+          proxyReq.setHeader('referer', 'https://www.bilibili.com/'); // 防爬虫
+        }
+      }
+    }
+  ]
+};
